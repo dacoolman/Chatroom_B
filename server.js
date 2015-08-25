92f8398ad96cdf7c0933fe37885ffa09e122d242
Michael Berger <MichaelBerger@Michaels-MacBook-Pro-2.local>
2015-08-24 22:23:17 -0700

    Styled and formatted

--- server.js
+++ server.js
@@ -1,50 +1,28 @@
 var users = {};
 var comments = [];
 var express = require('express');
-
 var app = express();
-
-
-app.get("/", function(request, response){
-
-response.send('Hello Express');
-
-
-});
-
 app.use(express.static(__dirname + "/static"));
-
 // This sets the location where express will look for the ejs views
 app.set('views', __dirname + '/views');
 // Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
 app.set('view engine', 'ejs');
-
-app.get("/users", function (request, response){
+app.get("/", function (request, response){
     // hard-coded user data
-  
-    response.render('users');
+    response.render('index');
 })
-
 var server = app.listen(8000, function(){
 	console.log('listening on port 8000')
-	
 });;
-
 var io = require('socket.io').listen(server);
-
 io.sockets.on('connection', function (socket) {
   console.log("WE ARE USING SOCKETS!");
   console.log(socket.id);
-
-
   socket.on('enter_user', function(data){
-
-users[data.name] = socket.id
-
-  
-    socket.emit('update_user', {comments: comments} );
+  users[data.name] = socket.id
+  socket.emit('update_user', {comments: comments} );
   });
-
+//Possible implmentation of disconnect:
 // socket.on('disconnect', function(){
 //        console.log(users);
 //        for (value in users)
@@ -71,13 +49,9 @@ socket.on('add_message', function(data){
       {temp[value] = data.message;
       comments.push(temp);
     break;}
-
   }
-
-
-io.emit('update_messages', {comments: comments});
+  io.emit('update_messages', {comments: comments});
 })
-
 })
 
 
