849f9398e05311f509ba1a670c4c5676d09fd37f
Michael Berger <MichaelBerger@Michaels-MacBook-Pro-2.local>
2015-08-28 16:26:23 -0700

    Styled

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ var users = {};
 var comments = [];
 var express = require('express');
 var app = express();
+
+
+app.get("/", function(request, response){
+    response.render('index');
+});
+
 app.use(express.static(__dirname + "/static"));
+
+
 // This sets the location where express will look for the ejs views
 app.set('views', __dirname + '/views');
 // Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
 app.set('view engine', 'ejs');
-app.get("/", function (request, response){
-    // hard-coded user data
-    response.render('index');
-})
-var server = app.listen(8000, function(){
+
+
+var port = process.env.PORT || 8000;
+var server = app.listen(port, function(){
   console.log('listening on port 8000')
 });;
+
+
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
   console.log("WE ARE USING SOCKETS!");
@@ -23,6 +32,7 @@ io.sockets.on('connection', function (socket) {
   socket.emit('update_user', {comments: comments} );
   });
 //Possible implmentation of disconnect:
+
 // socket.on('disconnect', function(){
 //        console.log(users);
 //        for (value in users)
@@ -38,19 +48,22 @@ io.sockets.on('connection', function (socket) {
 //         io.emit('update_user', users);
 //     });
 
-socket.on('add_message', function(data){
+
+  socket.on('add_message', function(data){
     //get name
     console.log('hi', users);
-  temp = {}
-  for (value in users)
-  {
-    console.log('hey', users[value], socket.id);
-    if (users[value] == socket.id)
-      {temp[value] = data.message;
-      comments.push(temp);
-    break;}
-  }
-  io.emit('update_messages', {comments: comments});
+    temp = {}
+    for (value in users)
+    {
+      console.log('hey', users[value], socket.id);
+      if (users[value] == socket.id)
+        {temp[value] = data.message;
+        comments.push(temp);
+      break;}
+
+    }
+    io.emit('update_messages', {comments: comments});
+
 })
 })
 
